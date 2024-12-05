1faf5dbfeb651ec7ff484a6f6731e2ebea0b8631
Jakub <jakubhanula0@gmail.com>
2024-12-05 22:24:32 +0100

    0.0.6.5

--- git.sh
+++ git.sh
@@ -7,7 +7,7 @@ if ! [ $1 = 0 ] && ! [ $1 = 1 ] && ! [ $1 = 2 ] && ! [ $1 = 3 ]; then
 fi
 
 
-oldVersion=$(head -n 1 version)
+oldv=$(head -n 1 version)
 
 v0=0
 v1=0
@@ -16,7 +16,7 @@ v3=0
 
 i=0
 
-for element in $(echo $oldVersion | tr "." "\n")
+for element in $(echo $oldv | tr "." "\n")
 do
   if [ $i = 0 ]; then
     v0=$element
@@ -56,13 +56,13 @@ if [ $1 = 3 ]; then
 fi
 
 
-newVersion=$v0.$v1.$v2.$v3
+newv=$v0.$v1.$v2.$v3
 echo $output > version
-echo "$oldVersion (old)"
-echo "$newVersion (new)"
+echo "$oldv (old)"
+echo "$newv (new)"
 
 git add .
-git commit -m $newVersion
+git commit -m $newv
 git push
 
 clear
