53886e1490dc7593cbc87d00f650704b9d17f9cd
Jakub <jakubhanula0@gmail.com>
2025-03-14 17:43:25 +0100

    0.5.2.7

--- src/index.js
+++ src/index.js
@@ -5,20 +5,22 @@ import { config, files } from "./lib/consts"
 import { openScene } from "./lib/utils"
 import "./index.css"
 
-const setUp = () => {
-  document.oncontextmenu = (event) => event.preventDefault()
+{
+  const setUp = () => {
+    document.oncontextmenu = (event) => event.preventDefault()
 
-  let scene = files
-  let lastKey = ``
-  for (const key of config.pathToMainScene.split(`.`).slice(1)) {
-    scene = scene[key]
-    lastKey = key
+    let scene = files
+    let lastKey = ``
+    for (const key of config.pathToMainScene.split(`.`).slice(1)) {
+      scene = scene[key]
+      lastKey = key
+    }
+
+    openScene(scene, lastKey)
   }
 
-  openScene(scene, lastKey)
+  setUp()
 }
-
-setUp()
 
 const root = createRoot(document.getElementsByTagName(`main`)[0])
 root.render(
