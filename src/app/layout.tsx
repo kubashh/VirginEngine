0671b7c4ba66bfa7de7e90820a0ba5cd3d863278
Jakub <jakubhanula0@gmail.com>
2025-07-14 14:47:36 +0200

    0.13.0.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import Head from "next/head"
 
 export const metadata: Metadata = {
   title: "VirginEngine",
@@ -9,6 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/virginengine/favicon.ico" />
+      </Head>
       <body className="overflow-hidden select-none text-white bg-black">{children}</body>
     </html>
   )
