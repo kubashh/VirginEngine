2cbaa548771784868d621e96d925e19af38a1f5d
Jakub <jakubhanula0@gmail.com>
2025-07-14 16:36:02 +0200

    0.13.0.9

--- core/src/components/GameObject.js
+++ core/src/components/GameObject.js
@@ -2,14 +2,12 @@ class GameObject {
   toUpdate = []
   toRender = []
 
-  constructor({ parent, transform, rect, text, sprite, start, update, render, name, ...rest }) {
+  constructor({ parent, transform, rect, text, sprite, start, update, render, ...rest }) {
     if (parent) this.parent = parent
 
     this.transform = new Transform(transform, this)
     if (text) this.text = new Text(text, this, rect)
     if (sprite) this.sprite = new Sprite(sprite, this)
-
-    if (name) this.name = name
 
     for (const key in rest) {
       if (isChildKey(key)) {
