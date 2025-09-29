d30ecfdf87fde44701351a80180c791e7b792514
Jakub <jakubhanula0@gmail.com>
2025-09-29 22:02:44 +0200

    update Docs and deploy.yaml

--- src/docs/Docs.tsx
+++ src/docs/Docs.tsx
@@ -3,11 +3,12 @@ import Window from "../components/Window"
 function DocsContent() {
   return (
     <div className="p-0.75 overflow-y-scroll select-text">
+      Visit:
       <a
         className="ml-2 hover:text-zinc-400"
-        href="https://github.com/kubashh/virginengine?tab=readme-ov-file"
+        href="https://github.com/VirginEngine/VirginEngine?tab=readme-ov-file"
       >
-        https://github.com/kubashh/virginengine?tab=readme-ov-file
+        https://github.com/VirginEngine/VirginEngine?tab=readme-ov-file
       </a>
     </div>
   )
