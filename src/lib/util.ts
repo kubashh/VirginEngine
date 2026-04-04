1f69c55b8ec075a5469f4015a5d00b1d06fa81f3
kubashh <jakubhanula0@gmail.com>
2026-04-04 17:11:21 +0200

    remove --production flag in buildBuild

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -5,18 +5,18 @@ export function deepCopy<T>(obj: T): T {
   return JSON.parse(JSON.stringify(obj));
 }
 
-function createElement({ name, ...props }: Any) {
-  const element = document.createElement(name);
-  for (const key in props) element[key] = props[key];
-  element.click();
-}
-
 export function downloadFile(name: string, text: string) {
   createElement({
     name: `a`,
     href: `data:text;charset=utf-8,${encodeURIComponent(text)}`,
     download: name,
   });
+}
+
+function createElement({ name, ...props }: Any) {
+  const element = document.createElement(name);
+  for (const key in props) element[key] = props[key];
+  element.click();
 }
 
 export function isValidName(name: string) {
@@ -161,30 +161,3 @@ export function getType(data: any) {
 //     img.src = src
 //   })
 // }
-
-export function optymalizeImageSrc(src: string, quality: number) {
-  const { resolve, promise } = Promise.withResolvers<string>();
-
-  if (quality === 1) resolve(src);
-
-  const img = new Image();
-
-  img.onload = () => {
-    const canvas = document.createElement(`canvas`);
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    const ctx = canvas.getContext(`2d`)!;
-    ctx.drawImage(img, 0, 0, img.width, img.height);
-
-    const newSrc = canvas.toDataURL(`image/jpeg`, quality);
-
-    console.log(src.length, newSrc.length);
-
-    resolve(newSrc.length < src.length ? newSrc : src);
-  };
-
-  img.src = src;
-
-  return promise;
-}
